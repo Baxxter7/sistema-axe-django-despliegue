66daebb643d476882b9e11dd32abcb6cb89213bb
Josué Isaías Martínez López <91571699+Baxxter7@users.noreply.github.com>
2023-05-01 18:57:05 -0600

    Agregando STATIC_ROOT

--- AXE/settings.py
+++ AXE/settings.py
@@ -145,8 +145,8 @@ CORS_ORIGIN_WHITELIST = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = BASE_DIR / 'staticfiles'
-#STATICFILES_SOTRAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_SOTRAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
